023b6c538d6cc84d18422b7aebf7607b72fe822e
Cole Faust <colecfaust@gmail.com>
2018-01-20 14:31:08 -0800

    Added code to load apikey from file

--- background.js
+++ background.js
@@ -1,2 +1,19 @@
 console.log("Loaded");
 
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    };
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+var apiKey;
+httpGetAsync(chrome.extension.getURL('apikey.txt'), function(text) {
+  apiKey = text;
+  console.log(apiKey);
+});
+
