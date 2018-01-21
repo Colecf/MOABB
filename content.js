27d9787113b33bd763712693a535b90ba7238495
Will Bolden <williamhbolden@gmail.com>
2018-01-20 19:58:15 -0800

    Start of updating censor on change

--- content.js
+++ content.js
@@ -26,7 +26,7 @@ $("*:visible").each(function() {
   var text = $(this).clone().children().remove().end().text();
 
   if(text.length > 10 && !isEmptyOrSpaces(text)) {
-    console.log(text);
+   // console.log(text);
     ofsarr.push(pagetext.length)
     pagetext += ("\n\n"+text)
     objarr.push($(this))
@@ -36,21 +36,20 @@ $("*:visible").each(function() {
 ofsarr.push(ofsarr[ofsarr.length-1]+1)
 
 
-console.log(pagetext)
-console.log(objarr)
+//console.log(pagetext)
+//console.log(objarr)
 
 
-console.log(objarr[0].clone().children().remove().end().text())
+//console.log(objarr[0].clone().children().remove().end().text())
 
 
-
+console.log(objarr.length)
 objarr.forEach(function(obj, i){
 	text = objarr[i].clone().children().remove().end().text();
 
 	analyzeSentiment(text, function(data){
-
 		censor(objarr[i], data.documentSentiment.score)
-		console.log(data, data.documentSentiment.score)
+//		console.log(data, data.documentSentiment.score)
 
 	})
 
@@ -58,3 +57,26 @@ objarr.forEach(function(obj, i){
 
 
 
+var observer = new MutationObserver(function(list){
+
+	console.log("srdfghfdxgfdgfdhfghgfhfghfghhg")
+
+	list.forEach(function(obj, i){
+		//if($(obj).is(':visible')){
+
+
+			text = $(obj).clone().children().remove().end().text();
+
+			console.log(text)
+			analyzeSentiment(text, function(data){
+				censor($(obj), data.documentSentiment.score)
+		//		console.log(data, data.documentSentiment.score)
+
+			})
+		//}
+	})
+})
+
+var config = {subtree:true, childList: true}
+
+observer.observe(document.body, config)
