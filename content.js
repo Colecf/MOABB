53130d20d69f63da6bf87d39237f3340f3e62df4
Will Bolden <williamhbolden@gmail.com>
2018-01-20 16:07:47 -0800

    Text bluring

--- content.js
+++ content.js
@@ -3,10 +3,53 @@ chrome.runtime.sendMessage("getapikey", function(response) {
   apiKey = response;
 });
 
-$("*").each(function() {
+
+var pagetext = ""
+var objarr = []
+
+$("*:not(script)").each(function() {
   var text = $(this).clone().children().remove().end().text();
+
+
   if(text.length > 0) {
     console.log(text);
+    pagetext += "\n\n"+text
+    objarr.push($(this))
+
+
+    //if(Math.random() > 0.5){
+    $(this).css("filter", "blur(6px)")
+
+    $(this).hover(
+    function () {
+        $(this).css("filter", "blur(0px)")
+    }, 
+    function () {
+        $(this).css("filter", "blur(6px)")
+    }
+    );
+
+    //$(this).css("hover", "filter: blur(0px)")
+//}
+    //this.style.filter = "blur(6px)"
   }
 });
+
+
+console.log(pagetext)
+console.log(objarr)
+
+analyzeSentiment(text, function(data){
+
+
+	sentences = data.sentences
+
+	obj_index = 0;
+
+	for(var i = 0; i < sentences.length; i++){
+
+	}
+
+
+})
 
