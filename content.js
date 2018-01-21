ade6670dc9ed4721e3182f511da4186d32be8250
Will Bolden <williamhbolden@gmail.com>
2018-01-20 18:37:45 -0800

    Slightly incorrect implementation of matching google results with objects

--- content.js
+++ content.js
@@ -1,63 +1,99 @@
 var apiKey = undefined;
 analyzeSentiment("I hate google.", function(response) {
   console.log(response);
+  console.log(response.sentences[0].text)
 });
 
 
-
-var pagetext = ""
-var objarr = []
 
 function analyzeSentiment(text, cb) {
   chrome.runtime.sendMessage({type: "analyzeSentiment", text: text}, cb);
 }
 
-$("*:not(script)").each(function() {
+function isEmptyOrSpaces(str){
+    return str === null || str.match(/^ *$/) !== null;
+}
+
+function censor(obj, sentiment){
+
+	//console.log(sentiment)
+	if(sentiment < -0.25){
+		$(obj).css("filter", "blur(6px)")
+
+	    $(obj).hover(
+	    function () {
+	        $(obj).css("filter", "blur(0px)")
+	    }, 
+	    function () {
+	        $(obj).css("filter", "blur(6px)")
+	    }
+	    );
+	}
+}
+
+
+var pagetext = ""
+var objarr = []
+var ofsarr = []
 
 
 
+$("*:not(script):visible").each(function() {
   var text = $(this).clone().children().remove().end().text();
 
-
-  if(text.length > 0) {
-
+  if(text.length > 10 && !isEmptyOrSpaces(text)) {
     console.log(text);
+    ofsarr.push(pagetext.length)
     pagetext += "\n\n"+text
     objarr.push($(this))
-
-
-    //if(Math.random() > 0.5){
-    $(this).css("filter", "blur(6px)")
-
-    $(this).hover(
-    function () {
-        $(this).css("filter", "blur(0px)")
-    }, 
-    function () {
-        $(this).css("filter", "blur(6px)")
-    }
-    );
-
-    //$(this).css("hover", "filter: blur(0px)")
-//}
-    //this.style.filter = "blur(6px)"
-
   }
 });
+
+ofsarr.push(ofsarr[ofsarr.length-1]+1)
 
 
 console.log(pagetext)
 console.log(objarr)
 
-analyzeSentiment(text, function(data){
 
+console.log(objarr[0].clone().children().remove().end().text())
 
-	sentences = data.sentences
+analyzeSentiment(pagetext, function(data){
 
-	obj_index = 0;
+	console.log(data)
 
-	for(var i = 0; i < sentences.length; i++){
+	var sentence_index = 0
 
+	for(var i = 0; i < objarr.length; i++){
+
+		var total_sentiment = 0;
+		var total_score = 0;
+		var total_magnitude = 0;
+
+		console.log(":::::",objarr[i].clone().children().remove().end().text(), ":::::")		
+
+		while(true) {
+			sentence = data.sentences[sentence_index]
+
+			console.log(sentence.text.beginOffset, ofsarr[i+1])
+
+			if(sentence.text.beginOffset >= ofsarr[i+1]){
+				break;
+			}
+			console.log("\t\t\t",sentence.text.content)
+
+			sentence_index++
+
+			total_sentiment += sentence.sentiment.score * sentence.sentiment.magnitude;
+			total_magnitude += sentence.sentiment.magnitude;
+			total_score += sentence.sentiment.score
+			
+		}
+
+		if (total_magnitude == 0){
+			total_magnitude = 1
+		}
+		censor(objarr[i], total_sentiment)
 	}
 
 
